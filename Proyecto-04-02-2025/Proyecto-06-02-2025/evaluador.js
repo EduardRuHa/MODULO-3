cf6fe871fa3b661343daad43942017847690e8fa
EduardRuHa <ruizhihataro.21@gmail.com>
2025-02-26 00:24:28 -0600

    Proyecto-06-02-2025

---
+++ Proyecto-04-02-2025/Proyecto-06-02-2025/evaluador.js
@@ -0,0 +1,13 @@
+// Se le da valor a la variable nota y asignamos un valor numérico (se puede cambiar por cualquier valor entre 0 y 100)
+let nota = 85;  // Se puede cambiar este valor para probar diferentes resultados
+
+// Evaluamos el valor de la nota usando estructuras de control if, else if y else
+if (nota >= 90) {
+    console.log("Excelente");
+} else if (nota >= 75) {
+    console.log("Bien");
+} else if (nota >= 60) {
+    console.log("Suficiente");
+} else {
+    console.log("No aprueba");
+}
